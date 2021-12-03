cfb5a070cce751a1fb6e31c761d31067f0a4e84c
Murat Karasah <mkarasah@gmail.com>
2021-12-03 14:38:48 +0300

    03/12/2021 the end

--- stringYapilari/Program.cs
+++ stringYapilari/Program.cs
@@ -6,7 +6,43 @@ namespace stringYapilari
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //string cumle =  "        Merhaba  dünya  ";
+
+            ////REPLACE
+            //string yenicumle = cumle.Replace(cumle,"nasılsın");
+
+            ////Remove
+            //string yenicumle = cumle.Remove(0, 3);
+
+            ////Substring
+            //string yenicumle = cumle.Substring(2,3);
+
+            ////trim-trimend-trimstart
+            ////Trim sağ ve soldaki boşlukları siler
+            //string yenicumle = cumle.Trim();
+            ////trimEnd bitiş boşlukları
+            ////trimstart baş boşluklar
+            //string yenicumle = cumle.TrimEnd();
+
+            //// ToLower - To Upper
+            //string yenicumle = cumle.ToLower();
+            //string yenicumle = cumle.ToUpper();
+
+            ////Lenght
+            //int yenicumle = Convert.ToInt32(cumle.Length); 
+            //Console.WriteLine(yenicumle);
+
+            //Split
+            string cumle = "çorba,pilav,nout";
+            for (int i = 0; i < cumle.Split(',').Length; i++)
+            {
+                Console.WriteLine( cumle.Split(',')[i]);
+            }
+
+
+
+
+
         }
     }
 }
